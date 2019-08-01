9c84dfac10c0b903372a136e93af3402bd3c04f3
mac <mac@shiva--R.local>
2019-08-01 17:28:12 +0530

    first commit

---
+++ collectionTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionTableview
+//
+//  Created by mac on 8/1/19.
+//  Copyright © 2019 shiva+R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
